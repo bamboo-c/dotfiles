3a04b4e0145541632c06051f4d2bc8a4b043e0c1
komei otake <komei1220@gmail.com>
2017-10-20 22:33:13 +0900

    update

--- .elisp/conf/init-yasnippet.el
+++
@@ -1,8 +0,0 @@
-;; Yasnippet
-; http://code.google.com/p/yasnippet/
-; http://blog.clouder.jp/archives/001037.html
-(require 'yasnippet)
-(yas/initialize)
-(yas/load-directory "~/dotfiles/.elisp/snippets")
-
-
